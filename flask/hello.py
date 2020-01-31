09e4f26a0e443de71ddcf6c238e9e55b6b1d351d
Juan Catalan <jscatalan@falabella.cl>
2020-01-31 19:32:57 -0300

    Change flask to rest

--- flask/hello.py
+++ flask/hello.py
@@ -1,9 +1,15 @@
+# app.py - a minimal flask api using flask_restful
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-if __name__ == "__main__":
-    app.run()
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
